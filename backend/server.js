f2533241861568ebc7288e09db7060e707d0636b
Giigauri <gigauridavit26@gmail.com>
2020-11-21 22:36:13 +0400

    Models & Seeder

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/DB.js'
+import Products from './Data/products.js'
+import colors from 'colors'
+import morgan from 'morgan'
 
-const Products = require('./Data/products')
+dotenv.config()
+
+connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.get('/api/products', (req, res) => {
     res.json(Products)
@@ -14,4 +25,9 @@ app.get('/api/products/:id', (req, res) => {
     res.json(Product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+)
