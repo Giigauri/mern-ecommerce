f47ab3f1cda4d296eeb13b774e38a98ffb692185
Giigauri <gigauridavit26@gmail.com>
2020-11-28 20:27:44 +0400

    Frontend Register Services

--- frontend/src/redux/ReduxStore.js
+++ frontend/src/redux/ReduxStore.js
@@ -5,18 +5,27 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 // Application Reducer
 import { productListReducer, productDetailsReducer } from './products/productReducer'
 import { cartReducer } from './cart/cartReducer'
+import { userLoginReducer, userRegisterReducer } from './user/userReducer'
 
 const rootReducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') 
-    ? JSON.parse(localStorage.getItem('cartItems')): []
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : []
+
+const userInfoFromStorage = localStorage.getItem('userInfo') 
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null
 
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage }
+    cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage }
 }
 
 const middleware = [thunk]
